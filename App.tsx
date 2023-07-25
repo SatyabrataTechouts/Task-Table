cb644da6589f94e9bce7540e9df175b3af8d8425
satya <satyabrata.b@techouts.co.in>
2023-07-25 17:34:31 +0530

    add

--- App.tsx
+++ App.tsx
@@ -1,17 +1,40 @@
+import notifee from '@notifee/react-native';
 import React from 'react';
 import { Pressable, Text, View } from 'react-native';
 import BackgroundService from 'react-native-background-actions';
 import Home from './src/components/Home';
-
-const sleep = (time: any) => new Promise<void>((resolve) => setTimeout(() => resolve(), time));
-
-
+const sleep = (time: any) =>
+  new Promise<void>(resolve => setTimeout(() => resolve(), time));
 
 const App = () => {
+  async function onDisplayNotification() {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
   const veryIntensiveTask = async (taskDataArguments: any) => {
     // Example of an infinite loop task
     const { delay } = taskDataArguments;
-    await new Promise(async (resolve) => {
+    await new Promise(async resolve => {
       for (let i = 0; BackgroundService.isRunning(); i++) {
         console.log(i);
         await sleep(delay);
@@ -35,29 +58,47 @@ const App = () => {
 
   const startBackGround = async () => {
     await BackgroundService.start(veryIntensiveTask, options);
-    await BackgroundService.updateNotification({ taskDesc: 'New ExampleTask description' });
-  }
+    await BackgroundService.updateNotification({
+      taskDesc: 'New ExampleTask description',
+    });
+  };
   const stopBackgroundService = async () => {
     await BackgroundService.stop();
-  }
+  };
   return (
     <>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 12 }}>
-        <Pressable onPress={startBackGround} style={{
-          height: 40, width: 120, backgroundColor: 'green',
-          alignItems: 'center',
-          justifyContent:'center'
-        }}><Text>Start</Text></Pressable>
-        <Pressable onPress={stopBackgroundService} 
-         style={{
-          height: 40, width: 120, backgroundColor: 'red',
-          alignItems: 'center',
-          justifyContent:'center'
-        }}><Text>Stop</Text></Pressable>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-around',
+          paddingVertical: 12,
+        }}>
+        <Pressable
+          onPress={()=>startBackGround()}
+          style={{
+            height: 40,
+            width: 120,
+            backgroundColor: 'green',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text>Start</Text>
+        </Pressable>
+        <Pressable
+          onPress={() =>  stopBackgroundService() }
+          style={{
+            height: 40,
+            width: 120,
+            backgroundColor: 'red',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}>
+          <Text>Stop</Text>
+        </Pressable>
       </View>
       <Home />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,72 +1,73 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
-
-
-
-const TableRow = ({number, name, age, occupation }:any) => {
-    return (
-      <View style={styles.row}>
-        <Text style={styles.cell}>{number}</Text>
-        <Text style={styles.cell}>{name}</Text>
-        <Text style={styles.cell}>{age}</Text>
-        <Text style={styles.cell}>{occupation}</Text>
-      </View>
-    );
-  };
-const Home =  () => {
-    
-   
-    
+const TableRow = ({number, name, age, occupation}: any) => {
   return (
-
-    <ScrollView >
-    <ScrollView horizontal>
-    <View style={styles.table}>
-      <TableRow number={"number"} name="Name" age="Age" occupation="occupation" />
-      <TableRow number={1} name="John" age="30" occupation="Engineer" />
-      <TableRow number={2} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={3} name="Bob" age="35" occupation="Developer" />
-      <TableRow number={4} name="John" age="30" occupation="Engineer" />
-      <TableRow number={5} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={6} name="Bob" age="35" occupation="Developer" />
-      <TableRow number={7} name="John" age="30" occupation="Engineer" />
-      <TableRow number={8} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={9} name="Bob" age="35" occupation="Developer" />
-      <TableRow number={10} name="John" age="30" occupation="Engineer" />
-      <TableRow number={11} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={12} name="Bob" age="35" occupation="Developer" />
-      <TableRow number={13} name="John" age="30" occupation="Engineer" />
-      <TableRow number={14} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={15} name="Bob" age="35" occupation="Developer" />
-      <TableRow number={16} name="John" age="30" occupation="Engineer" />
-      <TableRow number={17} name="Jane" age="28" occupation="Designer" />
-      <TableRow number={18} name="Bob" age="35" occupation="Developer" />
-     
+    <View style={styles.row}>
+      <Text style={styles.cell}>{number}</Text>
+      <Text style={styles.cell}>{name}</Text>
+      <Text style={styles.cell}>{age}</Text>
+      <Text style={styles.cell}>{occupation}</Text>
     </View>
+  );
+};
+const Home = () => {
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        <View style={styles.table}>
+          <TableRow
+            number={'number'}
+            name="Name"
+            age="Age"
+            occupation="occupation"
+          />
+          <TableRow number={1} name="John" age="30" occupation="Engineer" />
+          <TableRow number={2} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={3} name="Bob" age="35" occupation="Developer" />
+          <TableRow number={4} name="John" age="30" occupation="Engineer" />
+          <TableRow number={5} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={6} name="Bob" age="35" occupation="Developer" />
+          <TableRow number={7} name="John" age="30" occupation="Engineer" />
+          <TableRow number={8} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={9} name="Bob" age="35" occupation="Developer" />
+          <TableRow number={10} name="John" age="30" occupation="Engineer" />
+          <TableRow number={11} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={12} name="Bob" age="35" occupation="Developer" />
+          <TableRow number={13} name="John" age="30" occupation="Engineer" />
+          <TableRow number={14} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={15} name="Bob" age="35" occupation="Developer" />
+          <TableRow number={16} name="John" age="30" occupation="Engineer" />
+          <TableRow number={17} name="Jane" age="28" occupation="Designer" />
+          <TableRow number={18} name="Bob" age="35" occupation="Developer" />
+        </View>
+      </ScrollView>
     </ScrollView>
-    </ScrollView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 const styles = StyleSheet.create({
-    table: {
-      borderWidth: 1,
-      borderColor: '#000',
-      width:900
-    },
-    row: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      padding: 10,
-      borderBottomWidth: 1,
-      borderBottomColor: '#000',
-      
-    },
-    cell: {
-      flex: 1,
-      textAlign: 'center',
-      borderRightWidth:1,
-    
-    },
-  });
+  table: {
+    borderTopWidth:1,
+    borderBottomWidth:1,
+    borderLeftWidth:1,
+   // borderWidth: 1,
+    borderColor: '#000',
+    width: 900,
+  
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    // padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#000',
+   
+  },
+  cell: {
+    flex: 1,
+    textAlign: 'center',
+    borderRightWidth: 1,
+    padding: 10,
+  },
+});
